8dd3eaa5dfcf0775491eef5c15980ba4b6627c09
Rishi <Rishi_Singhal@McAfee.com>
2021-11-17 12:48:31 +0530

    feat: update bundle_3_2.min.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     main: lModules,
   },
   output: {
-    filename: "bundle_3_1.min.js",
+    filename: "bundle_3_2.min.js",
     path: path.resolve(__dirname, "dist"),
   },
   module: {
