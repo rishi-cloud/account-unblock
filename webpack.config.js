3f22a657875c5a765c3b07cfdea74cd66062d7a7
atulrana007 <atul.rana@zopsmart.com>
2021-12-02 13:10:51 +0530

    update bundle 5_5

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     main: lModules,
   },
   output: {
-    filename: "bundle_5_4.min.js",
+    filename: "bundle_5_5.min.js",
     path: path.resolve(__dirname, "dist"),
   },
   module: {
